7b3a98579295ba3ce0d049223e01cc1713865e0d
md.rubel <md.rubel@HOAP2406101764A>
2023-02-07 07:16:20 +0600

    Add View Model Class Library And Add Model

--- OrderManagement.Entity/Models/Order.cs
+++ OrderManagement.Entity/Models/Order.cs
@@ -1,8 +1,23 @@
-﻿namespace OrderManagement.Entity.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderManagement.Entity.Models
 {
     public  class Order
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        
+        public int DeletedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+
+        public Boolean IsActive { get; set; }
+        public Boolean IsDeleted { get; set; }
+
     }
 }
---
+++ OrderManagementViewModel/ViewModels/SalesOrder/OrderViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementViewModel.ViewModels.SalesOrder
+{
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string CreatedByName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UpdatedByName { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+        public int DeletedByName { get; set; }
+        public DateTime DeletedDate { get; set; }
+
+        public Boolean IsActive { get; set; }
+        public Boolean IsDeleted { get; set; }
+    }
+}
