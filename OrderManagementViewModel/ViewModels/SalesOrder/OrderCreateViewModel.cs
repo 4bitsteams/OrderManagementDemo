8f64e23756627d635e6f02022456feb58e8c081f
md.rubel <md.rubel@HOAP2406101764A>
2023-02-07 07:18:51 +0600

    Create a OrderViewModel

---
+++ OrderManagementViewModel/ViewModels/SalesOrder/OrderCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementViewModel.ViewModels.SalesOrder
+{
+    public  class OrderCreateViewModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public Boolean IsActive { get; set; }
+    }
+}
