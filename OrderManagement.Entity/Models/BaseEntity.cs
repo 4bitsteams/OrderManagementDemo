4f40cdf024de89205964b02ff34b175a8d8a7e8f
md.rubel <md.rubel@HOAP2406101764A>
2023-02-13 11:41:23 +0600

    Separate Entity Base Class From Order

---
+++ OrderManagement.Entity/Models/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Entity.Models
+{
+    public  class BaseEntity
+    {
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+        public int DeletedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+
+        public Boolean IsActive { get; set; }
+        public Boolean IsDeleted { get; set; }
+    }
+}
--- OrderManagement.Entity/Models/Order.cs
+++ OrderManagement.Entity/Models/Order.cs
@@ -2,22 +2,11 @@
 
 namespace OrderManagement.Entity.Models
 {
-    public  class Order
+    public  class Order : BaseEntity
     {
         [Key]
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-
-        public int CreatedBy { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public int UpdatedBy { get; set; }
-        public DateTime UpdatedDate { get; set; }
-        
-        public int DeletedBy { get; set; }
-        public DateTime DeletedDate { get; set; }
-
-        public Boolean IsActive { get; set; }
-        public Boolean IsDeleted { get; set; }
 
     }
 }
